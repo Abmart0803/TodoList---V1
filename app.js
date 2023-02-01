a44debd394420da08f123fa6c080e1167e9fc97a
Abiola Martins <hsemartins@gmail.com>
2023-02-01 19:39:14 +0300

    Adede file

--- app.js
+++ app.js
@@ -2,31 +2,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 //set app to use ejs, the code is from ejs.co and let it be place immediately you declare your add
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
 
-var today = new Date();
+let today = new Date();
 
-var options = {
+let options = {
   weekday: "long",
   day: "numeric",
   month: "long"
 };
 
-var day = today.toLocaleDateString("en-US", options);
+let day = today.toLocaleDateString("en-US", options);
 
-res.render("list", {kindOfDay: day});
-
-app.post("/", function(req, res){
-  var item = req.body.newItem;
-  console.log(item);
-})
-  
+res.render("list", {listTitle: day, newListItems: items}); 
 });
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
